6ffe507329d365216efbb15ecdc3d5e933ed1e76
Chris Russell <chris.russell@clarlabs.com>
2025-10-12 23:23:54 +0300

    fix: organised imports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { Accordion } from './components/Accordion'
-export type { AccordionProps, AccordionItemProps } from './components/Accordion'
+export type { AccordionItemProps, AccordionProps } from './components/Accordion'
 
 export { Alert } from './components/Alert'
 export type { AlertProps, AlertVariant } from './components/Alert'
@@ -8,16 +8,16 @@ export { Avatar } from './components/Avatar'
 export type { AvatarProps, AvatarSize } from './components/Avatar'
 
 export { Badge } from './components/Badge'
-export type { BadgeProps, BadgeVariant, BadgeSize } from './components/Badge'
+export type { BadgeProps, BadgeSize, BadgeVariant } from './components/Badge'
 
 export { Breadcrumbs } from './components/Breadcrumbs'
-export type { BreadcrumbsProps, BreadcrumbItem } from './components/Breadcrumbs'
+export type { BreadcrumbItem, BreadcrumbsProps } from './components/Breadcrumbs'
 
 export { Button } from './components/Button'
-export type { ButtonProps, ButtonVariant, ButtonSize } from './components/Button'
+export type { ButtonProps, ButtonSize, ButtonVariant } from './components/Button'
 
 export { ButtonGroup } from './components/ButtonGroup'
-export type { ButtonGroupProps, ButtonGroupOrientation } from './components/ButtonGroup'
+export type { ButtonGroupOrientation, ButtonGroupProps } from './components/ButtonGroup'
 
 export { Card } from './components/Card'
 export type { CardProps } from './components/Card'
@@ -26,25 +26,25 @@ export { Checkbox } from './components/Checkbox'
 export type { CheckboxProps } from './components/Checkbox'
 
 export { Combobox } from './components/Combobox'
-export type { ComboboxProps, ComboboxOption } from './components/Combobox'
+export type { ComboboxOption, ComboboxProps } from './components/Combobox'
 
 export { Datagrid } from './components/Datagrid'
-export type { DatagridProps, DatagridColumn } from './components/Datagrid'
+export type { DatagridColumn, DatagridProps } from './components/Datagrid'
 
 export { DatePicker } from './components/DatePicker'
 export type { DatePickerProps } from './components/DatePicker'
 
 export { DateRangePicker } from './components/DateRangePicker'
-export type { DateRangePickerProps, DateRange } from './components/DateRangePicker'
+export type { DateRange, DateRangePickerProps } from './components/DateRangePicker'
 
 export { Divider } from './components/Divider'
-export type { DividerProps, DividerOrientation, DividerVariant } from './components/Divider'
+export type { DividerOrientation, DividerProps, DividerVariant } from './components/Divider'
 
 export { Dropdown } from './components/Dropdown'
-export type { DropdownProps, DropdownOption } from './components/Dropdown'
+export type { DropdownOption, DropdownProps } from './components/Dropdown'
 
 export { FilePicker } from './components/FilePicker'
-export type { FilePickerProps, FileItem, FileStatus } from './components/FilePicker'
+export type { FileItem, FilePickerProps, FileStatus } from './components/FilePicker'
 
 export { Header } from './components/Header'
 export type { HeaderProps } from './components/Header'
@@ -56,7 +56,7 @@ export { Label } from './components/Label'
 export type { LabelProps, LabelSize } from './components/Label'
 
 export { List } from './components/List'
-export type { ListProps, ListItem } from './components/List'
+export type { ListItem, ListProps } from './components/List'
 
 export { Modal } from './components/Modal'
 export type { ModalProps, ModalSize } from './components/Modal'
@@ -65,7 +65,7 @@ export { Pagination } from './components/Pagination'
 export type { PaginationProps } from './components/Pagination'
 
 export { ProgressBar } from './components/ProgressBar'
-export type { ProgressBarProps, ProgressBarVariant, ProgressBarSize } from './components/ProgressBar'
+export type { ProgressBarProps, ProgressBarSize, ProgressBarVariant } from './components/ProgressBar'
 
 export { RadioButton } from './components/RadioButton'
 export type { RadioButtonProps } from './components/RadioButton'
@@ -74,79 +74,79 @@ export { RangeInput } from './components/RangeInput'
 export type { RangeInputProps } from './components/RangeInput'
 
 export { RichSelect } from './components/RichSelect'
-export type { RichSelectProps, RichSelectOption } from './components/RichSelect'
+export type { RichSelectOption, RichSelectProps } from './components/RichSelect'
 
 export { SidePanel } from './components/SidePanel'
-export type { SidePanelProps, SidePanelPosition, SidePanelSize } from './components/SidePanel'
+export type { SidePanelPosition, SidePanelProps, SidePanelSize } from './components/SidePanel'
 
 export { Signpost } from './components/Signpost'
-export type { SignpostProps, SignpostPosition } from './components/Signpost'
+export type { SignpostPosition, SignpostProps } from './components/Signpost'
 
 export { Spinner } from './components/Spinner'
 export type { SpinnerProps, SpinnerSize, SpinnerVariant } from './components/Spinner'
 
 export { StackView } from './components/StackView'
-export type { StackViewProps, StackViewDirection, StackViewAlign, StackViewJustify, StackViewSpacing } from './components/StackView'
+export type { StackViewAlign, StackViewDirection, StackViewJustify, StackViewProps, StackViewSpacing } from './components/StackView'
 
 export { Stepper } from './components/Stepper'
-export type { StepperProps, StepperStep, StepperOrientation } from './components/Stepper'
+export type { StepperOrientation, StepperProps, StepperStep } from './components/Stepper'
 
 export { Table } from './components/Table'
-export type { TableProps, TableColumn } from './components/Table'
+export type { TableColumn, TableProps } from './components/Table'
 
 export { Tabs } from './components/Tabs'
-export type { TabsProps, TabItem, TabsVariant } from './components/Tabs'
+export type { TabItem, TabsProps, TabsVariant } from './components/Tabs'
 
 export { Textarea } from './components/Textarea'
 export type { TextareaProps, TextareaSize, TextareaVariant } from './components/Textarea'
 
 export { Timeline } from './components/Timeline'
-export type { TimelineProps, TimelineItem, TimelineOrientation } from './components/Timeline'
+export type { TimelineItem, TimelineOrientation, TimelineProps } from './components/Timeline'
 
 export { ToggleSwitch } from './components/ToggleSwitch'
 export type { ToggleSwitchProps, ToggleSwitchSize } from './components/ToggleSwitch'
 
 export { Tooltip } from './components/Tooltip'
-export type { TooltipProps, TooltipPosition } from './components/Tooltip'
+export type { TooltipPosition, TooltipProps } from './components/Tooltip'
 
 export { ToastProvider, useToast } from './components/Toast'
-export type { ToastProviderProps, ToastOptions, ToastVariant, ToastPosition } from './components/Toast'
+export type { ToastOptions, ToastPosition, ToastProviderProps, ToastVariant } from './components/Toast'
 
 export { TreeView } from './components/TreeView'
-export type { TreeViewProps, TreeNode } from './components/TreeView'
+export type { TreeNode, TreeViewProps } from './components/TreeView'
 
 export { VerticalNav } from './components/VerticalNav'
-export type { VerticalNavProps, NavItem, NavOrientation } from './components/VerticalNav'
+export type { NavItem, NavOrientation, VerticalNavProps } from './components/VerticalNav'
 
 export { Select } from './components/Select'
-export type { SelectProps, SelectOption, SelectSize, SelectVariant } from './components/Select'
+export type { SelectOption, SelectProps, SelectSize, SelectVariant } from './components/Select'
 
 export { Grid, GridItem } from './components/Grid'
-export type { GridProps, GridItemProps, GridColumns, GridGap, GridAlign, GridJustify } from './components/Grid'
+export type { GridAlign, GridColumns, GridGap, GridItemProps, GridJustify, GridProps } from './components/Grid'
 
 export { Container } from './components/Container'
-export type { ContainerProps, ContainerSize, ContainerPadding } from './components/Container'
+export type { ContainerPadding, ContainerProps, ContainerSize } from './components/Container'
 
 export { Spacer } from './components/Spacer'
-export type { SpacerProps, SpacerSize, SpacerAxis } from './components/Spacer'
+export type { SpacerAxis, SpacerProps, SpacerSize } from './components/Spacer'
 
-export { Skeleton, SkeletonText, SkeletonCircle, SkeletonRectangle } from './components/Skeleton'
-export type { SkeletonProps, SkeletonVariant, SkeletonAnimation } from './components/Skeleton'
+export { Skeleton, SkeletonCircle, SkeletonRectangle, SkeletonText } from './components/Skeleton'
+export type { SkeletonAnimation, SkeletonProps, SkeletonVariant } from './components/Skeleton'
 
 export { Video } from './components/Video'
-export type { VideoProps, VideoAspectRatio } from './components/Video'
+export type { VideoAspectRatio, VideoProps } from './components/Video'
 
 export { Audio } from './components/Audio'
 export type { AudioProps, AudioVariant } from './components/Audio'
 
 export { TagInput } from './components/TagInput'
-export type { TagInputProps, Tag, TagInputSize, TagInputVariant } from './components/TagInput'
+export type { Tag, TagInputProps, TagInputSize, TagInputVariant } from './components/TagInput'
 
 export { PinInput } from './components/PinInput'
-export type { PinInputProps, PinInputSize, PinInputVariant, PinInputType } from './components/PinInput'
+export type { PinInputProps, PinInputSize, PinInputType, PinInputVariant } from './components/PinInput'
 
 export { Rating } from './components/Rating'
-export type { RatingProps, RatingSize, RatingPrecision } from './components/Rating'
+export type { RatingPrecision, RatingProps, RatingSize } from './components/Rating'
 
 export { Window } from './components/Window'
 export type { WindowProps, WindowSize, WindowVariant } from './components/Window'
@@ -155,37 +155,37 @@ export { Gauge } from './components/Gauge'
 export type { GaugeProps, GaugeSize, GaugeVariant } from './components/Gauge'
 
 export { ColorPicker } from './components/ColorPicker'
-export type { ColorPickerProps, ColorFormat } from './components/ColorPicker'
+export type { ColorFormat, ColorPickerProps } from './components/ColorPicker'
 
 export { ContextMenu } from './components/ContextMenu'
-export type { ContextMenuProps, ContextMenuItem } from './components/ContextMenu'
+export type { ContextMenuItem, ContextMenuProps } from './components/ContextMenu'
 
 export { DataList } from './components/DataList'
-export type { DataListProps, DataListItem, DataListOrientation, DataListSize } from './components/DataList'
+export type { DataListItem, DataListOrientation, DataListProps, DataListSize } from './components/DataList'
 
 export { StatCard } from './components/StatCard'
-export type { StatCardProps, StatCardVariant, StatCardSize, TrendDirection } from './components/StatCard'
+export type { StatCardProps, StatCardSize, StatCardVariant, TrendDirection } from './components/StatCard'
 
 export { Split } from './components/Split'
-export type { SplitProps, SplitDirection } from './components/Split'
+export type { SplitDirection, SplitProps } from './components/Split'
 
 export { ImageGallery } from './components/ImageGallery'
-export type { ImageGalleryProps, GalleryImage, ImageGalleryVariant, ImageGallerySize } from './components/ImageGallery'
+export type { GalleryImage, ImageGalleryProps, ImageGallerySize, ImageGalleryVariant } from './components/ImageGallery'
 
 export { ThemeToggle } from './components/ThemeToggle'
 export type { ThemeToggleProps } from './components/ThemeToggle'
 
 export { LanguageSelect } from './components/LanguageSelect'
-export type { LanguageSelectProps, Language } from './components/LanguageSelect'
+export type { Language, LanguageSelectProps } from './components/LanguageSelect'
 
 export { Chat } from './components/Chat'
-export type { ChatProps, ChatMessage } from './components/Chat'
+export type { ChatMessage, ChatProps } from './components/Chat'
 
 export { Calendar } from './components/Calendar'
-export type { CalendarProps, CalendarEvent } from './components/Calendar'
+export type { CalendarEvent, CalendarProps } from './components/Calendar'
 
 export { Hero } from './components/Hero'
-export type { HeroProps, HeroVariant, HeroSize, HeroAlignment } from './components/Hero'
+export type { HeroAlignment, HeroProps, HeroSize, HeroVariant } from './components/Hero'
 
 export { Footer } from './components/Footer'
-export type { FooterProps, FooterVariant, FooterLink, FooterSection, SocialLink } from './components/Footer'
+export type { FooterLink, FooterProps, FooterSection, FooterVariant, SocialLink } from './components/Footer'
